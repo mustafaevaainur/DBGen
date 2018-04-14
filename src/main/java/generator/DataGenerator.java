4b62796691ad1d8f5b6e1e864281f7f4f39b806b
Ainur <79215797496@yandex.ru>
2018-04-14 12:47:27 +0300

    change in the amount of data

--- src/main/java/generator/DataGenerator.java
+++ src/main/java/generator/DataGenerator.java
@@ -14,49 +14,18 @@ import java.util.List;
 
 public class DataGenerator {
 
-    private final int pigsNumber = 2000;
-    /*private final int operatorsNumber = OperatorGenerator.getOperatorsNumber();
-    private final int doneActionsNumber = 40;
-    private final int didNumber = 50;
-    private final int whyCanDoNumber = 40;*/
-
-   /* private final int tariffsActivationNumber = pigsNumber;
-    private final int optionsActivationNumber = pigsNumber;
-    private final int usagePerMonthNumber = pigsNumber;*/
+    private final int pigsNumber = 2000000;
 
     private final int pigsNameNumber = pigsNumber;
     private final int whatCanDoNumber = pigsNumber;
     private final int whatAlreadyDoneNumber = pigsNumber;
     private final int whyCanDoNumber = pigsNumber;
 
-    /*private List<Integer> usersId;
-    private List<Integer> operatorsId;
-    private List<Integer> optionsId;
-    private List<Integer> citiesId;
-    private List<Integer> tariffsId;
-
-    private List<OptionActivation> optionActivations;
-    private List<TariffActivation> tariffActivations;*/
-    /*private List<UsesOptionRel> usesOptionRels = new ArrayList<>();
-    private List<UsesTariffRel> usesTariffRels = new ArrayList<>();*/
 
     private IProduceFactory factory = new GenProduceFactory();
     private IExporter exporter = new CsvExporter();
 
     public void generate() {
-
-        /* generate data for document-oriented *//*
-        usersId = generateObject(User.class, pigsNumber);
-        operatorsId = generateObject(Operator.class, operatorsNumber);
-        optionsId = generateObject(Option.class, doneActionsNumber);
-        citiesId = generateObject(City.class, didNumber);
-        tariffsId = generateObject(Tariff.class, whyCanDoNumber);
-
-
-        *//* generate data for column-oriented *//*
-        tariffActivations = generateColumnObject(TariffActivation.class, tariffsActivationNumber);
-        optionActivations = generateColumnObject(OptionActivation.class, optionsActivationNumber);
-        generateColumnObject(UsagePerMonth.class, usagePerMonthNumber);*/
 
 
         /* generate data for graph */
@@ -64,20 +33,6 @@ public class DataGenerator {
         generateObject(WhatAlreadyDoneRel.class, whatAlreadyDoneNumber);
         generateObject(WhatCanDoRel.class, whatCanDoNumber);
         generateObject(WhyCanDoRel.class, whyCanDoNumber);
-
-        /*for (TariffActivation ta : tariffActivations) {
-            usesTariffRels.add(new UsesTariffRel(ta.getId(), ta.getTariffId()));
-        }
-        exporter.export(usesTariffRels);
-        tariffActivations.clear();
-        usesTariffRels.clear();
-
-        for (OptionActivation oa : optionActivations) {
-            usesOptionRels.add(new UsesOptionRel(oa.getId(), oa.getOptionId()));
-        }
-        exporter.export(usesOptionRels);
-        optionActivations.clear();
-        usesOptionRels.clear();*/
 
     }
 
@@ -89,11 +44,5 @@ public class DataGenerator {
             ids.add(tObj.getId());
         }
         return ids;
-    }
-
-    private <T extends DBObject> List<T> generateColumnObject(Class<T> tClass, int amount) {
-        List<T> tList = factory.produce(tClass, amount);
-        exporter.export(tList);
-        return tList;
     }
 }
